720ef4b78d1a60fee2098a401df421ce82e0dd6d
Alfredo Cano <alfrecanodiaz@gmail.com>
2017-05-30 16:42:32 -0400

    Corregido blog

--- blog.php
+++ blog.php
@@ -44,9 +44,9 @@
 
 						<section class="timeline">
 							<div class="timeline-body">
-								<div class="timeline-date">
+								<!-- <div class="timeline-date">
 									<h3 class="heading-primary"></h3>
-								</div>
+								</div> -->
 
 								<article class="timeline-box left post post-medium">
 									<div class="row">
@@ -59,11 +59,6 @@
 															<img class="img-responsive" src="img/blog/blog-lancamento.png" alt="">
 														</div>
 													</div>
-													<div>
-														<div class="img-thumbnail">
-															<img class="img-responsive" src="img/blog/blog-image-2.jpg" alt="">
-														</div>
-													</div>
 												</div>
 											</div>
 										</div>
@@ -72,7 +67,7 @@
 										<div class="col-md-12">
 
 											<div class="post-content">
-												<h4 class="heading-primary"><a href="blog-post.php">Lançamento da Membrana de Celulose da Empresa Bionext no Peru</a></h4>
+												<h4 class="heading-primary"><a href="#">Lançamento da Membrana de Celulose da Empresa Bionext no Peru</a></h4>
 												<p>Hoje faz dois meses que a Membrana de Celulose da empresa Bionext foi lançada no Peru.
 											<p>O evento contou com a participaçao do Cirurgião Plástico e speaker Dr. Miguel Davila e a presença de 40 médicos.</p>
 											<p>Na ocasião os participantes tiveram a oportunidade de conhecer mais sobre o produto e também compartilhar seus conhecimentos.</p>
@@ -93,11 +88,11 @@
 											</div>
 										</div>
 									</div>
-									<div class="row">
+									<!-- <div class="row">
 										<div class="col-md-12">
-											<a href="blog-post.php" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
+											<a href="#" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
 										</div>
-									</div>
+									</div> -->
 
 								</article>
 
@@ -112,11 +107,6 @@
 															<img class="img-responsive" src="img/blog/blog-treinamento.png" alt="">
 														</div>
 													</div>
-													<div>
-														<div class="img-thumbnail">
-															<img class="img-responsive" src="img/blog/blog-image-3.jpg" alt="">
-														</div>
-													</div>
 												</div>
 											</div>
 										</div>
@@ -125,7 +115,7 @@
 										<div class="col-md-12">
 
 											<div class="post-content">
-												<h4 class="heading-primary"><a href="blog-post.php">Treinamento sobre a nova geração de endopróteses</a></h4>
+												<h4 class="heading-primary"><a href="#">Treinamento sobre a nova geração de endopróteses</a></h4>
 												<p>No dia 07 de março aconteceu o treinamento para os médicos e residentes do Instituto Nacional de Traumatologia e Ortopedia - INTO,localizado na cidade de Rio de Janeiro/RJ. </p>
 	                                            <p>Nesta oportunidade, foi apresentada a nova geração de endopróteses, com informações sobre as características, vantagens, opções de montagens e técnica cirúrgica. </p>
 	                                            <p>O treinamento foi muito proveitoso e﻿ essencial para que os participantes estejam munidos de conhecimento para a utilização dos materiais nos procedimentos cirúrgicos.﻿</p>
@@ -146,66 +136,17 @@
 											</div>
 										</div>
 									</div>
-									<div class="row">
+									<!-- <div class="row">
 										<div class="col-md-12">
-											<a href="blog-post.php" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
+											<a href="#" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
 										</div>
-									</div>
+									</div> -->
 
 								</article>
 
-								<article class="timeline-box left post post-medium">
-									<div class="row">
-
-										<div class="col-md-12">
-											<div class="post-image">
-												<div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
-													<div>
-														<div class="img-thumbnail">
-															<img class="img-responsive" src="img/blog/blog-feridas.jpg" alt="">
-														</div>
-													</div>
-													<div>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-12">
-
-											<div class="post-content">
-												<h4 class="heading-primary"><a href="blog-post.php">Feridas na pele dos idosos: Quais os motivos?</a></h4>
-												<p>A passagem cronológica dos anos acontece com qualquer pessoa e os primeiros sinais começam a aparecer na pele. Com o avançar da idade, a espessura da epiderme-derme passa a sofrer uma redução e diminui a elasticidade em decorrência da perda… </p>
-											</div>
-
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-12">
-											<div class="post-meta">
-												<span><i class="fa fa-calendar"></i> Janeiro 24, 2016 </span><br>
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-12">
-											<div class="post-meta">
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-12">
-											<a href="blog-post.php" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
-										</div>
-									</div>
-
-								</article>
-
-								<div class="timeline-date">
+								<!-- <div class="timeline-date">
 									<h3 class="heading-primary"></h3>
-								</div>
+								</div> -->
 
 								<article class="timeline-box left post post-medium">
 									<div class="row">
@@ -218,11 +159,6 @@
 															<img class="img-responsive" src="img/blog/blog-cuidados.jpg" alt="">
 														</div>
 													</div>
-													<div>
-														<div class="img-thumbnail">
-															<img class="img-responsive" src="img/blog/blog-image-2.jpg" alt="">
-														</div>
-													</div>
 												</div>
 											</div>
 										</div>
@@ -231,7 +167,7 @@
 										<div class="col-md-12">
 
 											<div class="post-content">
-												<h4 class="heading-primary"><a href="blog-post.php">Cuidados diários com a pele.</a></h4>
+												<h4 class="heading-primary"><a href="#">Cuidados diários com a pele.</a></h4>
 												<p>Higiene É importante limpar a pele duas vezes ao dia, de manhã e à noite, para evitar o acúmulo de oleosidade e resíduos de maquiagem e outros produtos, poluentes e poeira. O acúmulo de sujeira na pele leva ao entupimento… </p>
 											</div>
 
@@ -250,11 +186,11 @@
 											</div>
 										</div>
 									</div>
-									<div class="row">
+									<!-- <div class="row">
 										<div class="col-md-12">
-											<a href="blog-post.php" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
+											<a href="#" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
 										</div>
-									</div>
+									</div> -->
 
 								</article>
 
@@ -269,11 +205,6 @@
 															<img class="img-responsive" src="img/blog/blog-tratamento.jpg" alt="">
 														</div>
 													</div>
-													<div>
-														<div class="img-thumbnail">
-															<img class="img-responsive" src="img/blog/blog-image-1.jpg" alt="">
-														</div>
-													</div>
 												</div>
 											</div>
 										</div>
@@ -282,7 +213,7 @@
 										<div class="col-md-12">
 
 											<div class="post-content">
-												<h4 class="heading-primary"><a href="blog-post.php">Tratamento de Pacientes com Feridas.</a></h4>
+												<h4 class="heading-primary"><a href="#">Tratamento de Pacientes com Feridas.</a></h4>
 												<p>Lesões são consequência de uma danificação na pele. A cura de feridas vem evoluindo desde 3000 a.C., época em que as feridas com sangramento eram curadas com cauterização. A aplicação de torniquete e a sutura foram também alternativas para a…</p>
 											</div>
 
@@ -301,17 +232,63 @@
 											</div>
 										</div>
 									</div>
-									<div class="row">
+									<!-- <div class="row">
 										<div class="col-md-12">
-											<a href="blog-post.php" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
+											<a href="#" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
 										</div>
-									</div>
+									</div> -->
 
 								</article>
 
-								<div class="timeline-date">
+								<!-- <div class="timeline-date">
 									<h3 class="heading-primary"><a href="#">Carregar mais...</a></h3>
-								</div>
+								</div> -->
+
+								<article class="timeline-box left post post-medium">
+									<div class="row">
+
+										<div class="col-md-12">
+											<div class="post-image">
+												<div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
+													<div>
+														<div class="img-thumbnail">
+															<img class="img-responsive" src="img/blog/blog-feridas.jpg" alt="">
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+
+											<div class="post-content">
+												<h4 class="heading-primary"><a href="#">Feridas na pele dos idosos: Quais os motivos?</a></h4>
+												<p>A passagem cronológica dos anos acontece com qualquer pessoa e os primeiros sinais começam a aparecer na pele. Com o avançar da idade, a espessura da epiderme-derme passa a sofrer uma redução e diminui a elasticidade em decorrência da perda… </p>
+											</div>
+
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<div class="post-meta">
+												<span><i class="fa fa-calendar"></i> Janeiro 24, 2016 </span><br>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<div class="post-meta">
+											</div>
+										</div>
+									</div>
+									<!-- <div class="row">
+										<div class="col-md-12">
+											<a href="#" class="btn btn-xs btn-primary pull-right">Ver mais...</a>
+										</div>
+									</div> -->
+
+								</article>
 
 							</div>
 
